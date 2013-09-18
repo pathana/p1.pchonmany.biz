f393157ed152bd01d4fd08db828fabd2f89105db
Pathana Chonmany <admin@pchonmany.com>
2013-09-17 23:46:49 -0400

    Added HTML and text for the project and a CSS file

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Welcome to P1
+<html>
+<head>
+<title>Welcome to P1</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<h1>Pathana Chonmany</h1>
+
+<p>
+
+Hello there! My name is Pathana, I am 21 years old and about to gradute college this coming Friday with my Bachelor of Science degree in Web Design & Interactive Media. I am planning on continuing my studies to get a Master's Degree. I initially intended to go to UMass Boston and master in IT there. But then I came across the IT graduate's program here at the Harvard Extension School and decided to give it a whirl and come here. I understand it will be a lot of work and I am definitely willing to put my all into my studies here. <p>
+I am a web designer and developer, but I can also do more than just web things. I consider myself an all around <i>techy</i>. In my free time, I enjoy bike riding, being outdoors, spending time with family, playing video games, and listening to music. I recently just ended my job at Cheeseboy in South Station as a Shift Manager, because of graduating school soon so I will no longer be in the Boston area for 3 months. 
+
+<p>
+
+<b>Code Editors:</b> Sublime Text + Notepad<br>
+
+</body>
